23bc7d651bf0aa634e37787bf2d722f0a3412d9a
Oliver Moran <oliver.moran@gmail.com>
2013-12-09 08:37:58 +0000

    remove setTimeout from tests

--- test/tests/CustomElements.js
+++ test/tests/CustomElements.js
@@ -1,89 +1,71 @@
-var CREATED = "createdCallback";
-var ENTERED_VIEW = "enteredViewCallback";
-var LEFT_VIEW = "leftViewCallback";
-var ATTR_CHANGED = "attributeChangedCallback";
-
-var ATTR_VAL = "ATTR_VAL";
+var FIXTURE = document.getElementById("qunit-fixture");
 
 var INNER_HTML_1 = "INNER_HTML_1";
 var INNER_HTML_2 = "INNER_HTML_2";
+var ATTR_VAL = "ATTR_VAL";
+var TEST_TAG = "x-test";
 
-var fixture = document.getElementById("qunit-fixture");
-
-function generateXTestPrototype(){
-	// create the x-lotto prototype
-	var XTestPrototype = Object.create(HTMLElement.prototype);
-	XTestPrototype.createdCallback = function() {
-		this.innerHTML = INNER_HTML_1;
-		this.lifeCycle = CREATED;
-	};
-	XTestPrototype.enteredViewCallback = function() {
-		this.lifeCycle = ENTERED_VIEW;
-	};
-	XTestPrototype.leftViewCallback = function() {
-		this.lifeCycle = LEFT_VIEW;
-	};
-	XTestPrototype.attributeChangedCallback = function() {
-		this.lifeCycle = ATTR_CHANGED;
-	};
-
-	// this will indicate the life cycle state during tests
-	XTestPrototype.lifeCycle = null;
-
-	return XTestPrototype;
-}
 
 test("Environment: APIs", function() {
 	ok(window.MutationObserver, "window.MutationObserver exists");
 	ok(document.register, "document.regster exists");
 });
 
+
 asyncTest("Custom Element: Callbacks", function() {
-	var XTestPrototype = generateXTestPrototype();
+	var XTestPrototype = Object.create(HTMLElement.prototype);
 
 	expect(4);
 
+	var node = document.createElement(TEST_TAG);
 	// register the x-lotto element with the document
-	var XTest = document.register("x-test", {
+	var XTest = document.register(TEST_TAG, {
 		prototype: XTestPrototype
 	});
+	FIXTURE.appendChild(node);
 
-	var node = document.createElement("x-test");
-	ok(node.lifeCycle === CREATED, "Custom element created callback");
-
-	fixture.appendChild(node);
-	setTimeout(function() {
-		ok(node.lifeCycle === ENTERED_VIEW, "Custom element entered view callback");
+	XTestPrototype.createdCallback = function() {
+		ok(this === node, "Custom element created callback");
+	};
+	
+	XTestPrototype.enteredViewCallback = function() {
+		ok(this === node, "Custom element entered view callback");
 		node.setAttribute("data-attr", ATTR_VAL);
-		setTimeout(function() {
-			ok(node.lifeCycle === ATTR_CHANGED, "Custom element attribute changed callback");
-			node.outerHTML = "";
-			setTimeout(function() {
-				ok(node.lifeCycle === LEFT_VIEW, "Custom element left view callback");
-				start();
-			});
-		});
-	});
+	};
+	
+	XTestPrototype.attributeChangedCallback = function() {
+		ok(this === node, "Custom element attribute changed callback");
+		node.outerHTML = "";
+	};
+
+	XTestPrototype.leftViewCallback = function() {
+		ok(this === node, "Custom element left view callback");
+		start();
+	};
 });
 
+
 test("Custom Element: Manipulation", function() {
-	var XTestPrototype = generateXTestPrototype();
+	var XTestPrototype = Object.create(HTMLElement.prototype);
+	XTestPrototype.createdCallback = function() {
+		this.innerHTML = INNER_HTML_1;
+	};
 
 	// register the x-lotto element with the document
-	var XTest = document.register("x-test", {
+	var XTest = document.register(TEST_TAG, {
 		prototype: XTestPrototype
 	});
 
-	var node = document.createElement("x-test");
-	fixture.appendChild(node);
+	var node = document.createElement(TEST_TAG);
+	FIXTURE.appendChild(node);
 
-	ok(document.querySelector("x-test"), "Custom element added");
-	ok(document.querySelector("x-test").innerHTML === INNER_HTML_1, "Custom element HTML set");
+	ok(document.querySelector(TEST_TAG), "Custom element added");
+	ok(document.querySelector(TEST_TAG).innerHTML === INNER_HTML_1, "Custom element HTML set");
 
 	node.innerHTML = INNER_HTML_2;
 	ok(node.innerHTML === INNER_HTML_2, "Custom element HTML changed");
 
 	node.outerHTML = "";
-	ok(document.querySelector("x-test") === null, "Custom element removed");
+	ok(document.querySelector(TEST_TAG) === null, "Custom element removed");
 
 });
