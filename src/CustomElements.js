a32cc9c25b4a71354d64903f61ee8b400530c599
Oliver Moran <oliver.moran@gmail.com>
2013-12-10 20:40:54 +0000

    add polyfill for Function.prototype.bind

--- src/CustomElements.js
+++ src/CustomElements.js
@@ -1,4 +1,32 @@
 /*
+ * Function.prototype.bind polyfill from the Mozilla Developer Network
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+ * License: http://creativecommons.org/publicdomain/zero/1.0/
+ */
+if (!Function.prototype.bind) {
+Function.prototype.bind = function (oThis) {
+  if (typeof this !== "function") {
+    // closest thing possible to the ECMAScript 5 internal IsCallable function
+    throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+  }
+
+  var aArgs = Array.prototype.slice.call(arguments, 1), 
+    fToBind = this, 
+    fNOP = function () {},
+    fBound = function () {
+      return fToBind.apply(
+        this instanceof fNOP && oThis ? this : oThis,
+        aArgs.concat(Array.prototype.slice.call(arguments)));
+      };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
+
+/*
  * Copyright 2013 The Polymer Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
@@ -14,7 +42,6 @@
  * @class Document
 */
 
-
 // normalise MutationObserver API
 if (!window.MutationObserver && window.WebKitMutationObserver) {
   window.MutationObserver = window.WebKitMutationObserver;
@@ -22,6 +49,7 @@ if (!window.MutationObserver && window.WebKitMutationObserver) {
 if (!window.MutationObserver) {
   throw new Error('The CustomElements polyfill requires MutationObserver support. Include the MutationObserver polyfill then include the CustomElements polyfill.');
 }
+
 
 (function(){
 
